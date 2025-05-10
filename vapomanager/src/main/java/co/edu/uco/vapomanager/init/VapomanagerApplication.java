7673ea819d661d89b56f20a5fa452f979f8ea33d
Jose Alejandro Valencia Henao <jose.valencia1373@uco.net.co>
2025-05-10 12:56:57 -0500

    carga linea base proyecto
    carga proyecto como linea de base con el scarffoding de una aplicacion
    web spring boot

---
+++ vapomanager/src/main/java/co/edu/uco/vapomanager/init/VapomanagerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.vapomanager.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapomanagerApplication.class, args);
+	}
+
+}
