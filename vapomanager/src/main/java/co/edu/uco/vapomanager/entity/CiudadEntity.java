18b6b4ff177b89e92011be322e181887c7cb8b4d
Simón Ochoa Ríos <simon08ochoa@gmail.com>
2025-05-12 16:19:55 -0500

    Arreglo

--- vapomanager/src/main/java/co/edu/uco/vapomanager/entity/CiudadEntity.java
+++ vapomanager/src/main/java/co/edu/uco/vapomanager/entity/CiudadEntity.java
@@ -6,60 +6,60 @@ import co.edu.uco.vapomanager.crosscutting.utilitarios.UtilObjeto;
 import co.edu.uco.vapomanager.crosscutting.utilitarios.UtilTexto;
 import co.edu.uco.vapomanager.crosscutting.utilitarios.UtilUUID;
 
-public final class CiudadEntity {
+public class CiudadEntity {
+	
+	public static final CiudadEntity DEFAULT_OBJECT = new CiudadEntity();
+	
+	private UUID id;
+	private String nombre;
+	private DepartamentoEntity departamento;
 
-    private UUID id;
-    private String nombre;
-    private DepartamentoEntity departamento;
+	public CiudadEntity() {
+		setId(UtilUUID.obtenerValorDefecto())
+		.setNombre(UtilTexto.getInstance().obtenerValorDefecto())
+		.setDepartamento(new DepartamentoEntity());
+	}
 
-    public static final CiudadEntity DEFAULT_OBJECT = new CiudadEntity();
+	public CiudadEntity(final UUID id) {
+		setId(id)
+		.setNombre(UtilTexto.getInstance().obtenerValorDefecto())
+		.setDepartamento(new DepartamentoEntity());
+	}
 
-    public CiudadEntity() {
-        setId(UtilUUID.obtenerValorDefecto());
-        setNombre(UtilTexto.getInstance().obtenerValorDefecto());
-        setDepartamento(new DepartamentoEntity());
-    }
+	public CiudadEntity(final UUID id, final String nombre, final DepartamentoEntity departamento) {
+		setId(id)
+		.setNombre(nombre)
+		.setDepartamento(departamento);
+	}
 
-    public CiudadEntity(final UUID id) {
-        setId(id);
-        setNombre(UtilTexto.getInstance().obtenerValorDefecto());
-        setDepartamento(new DepartamentoEntity());
-    }
+	public static CiudadEntity obtenerValorDefecto(final CiudadEntity ciudad) {
+		return UtilObjeto.getInstance().obtenerValorDefecto(ciudad, new CiudadEntity());
+	}
 
-    public CiudadEntity(final UUID id, final String nombre, final DepartamentoEntity departamento) {
-        setId(id);
-        setNombre(nombre);
-        setDepartamento(departamento);
-    }
+	public UUID getId() {
+		return id;
+	}
 
-    public static CiudadEntity obtenerValorDefecto(final CiudadEntity ciudad) {
-        return UtilObjeto.getInstance().obtenerValorDefecto(ciudad, DEFAULT_OBJECT);
-    }
+	public CiudadEntity setId(final UUID id) {
+		this.id = UtilUUID.obtenerValorDefecto(id);
+		return this;
+	}
 
-    public UUID getId() {
-        return id;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public CiudadEntity setId(final UUID id) {
-        this.id = UtilUUID.obtenerValorDefecto(id);
-        return this;
-    }
+	public CiudadEntity setNombre(final String nombre) {
+		this.nombre = UtilTexto.getInstance().quitarEspacioBlancoInicioFin(nombre);
+		return this;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public DepartamentoEntity getDepartamento() {
+		return departamento;
+	}
 
-    public CiudadEntity setNombre(final String nombre) {
-        this.nombre = UtilTexto.getInstance().quitarEspacioBlancoInicioFin(nombre);
-        return this;
-    }
-
-    public DepartamentoEntity getDepartamento() {
-        return departamento;
-    }
-
-    public CiudadEntity setDepartamento(final DepartamentoEntity departamento) {
-        this.departamento = DepartamentoEntity.obtenerValorDefecto(departamento);
-        return this;
-    }
+	public CiudadEntity setDepartamento(final DepartamentoEntity departamento) {
+		this.departamento = DepartamentoEntity.obtenerValorDefecto(departamento);
+		return this;
+	}
 }
